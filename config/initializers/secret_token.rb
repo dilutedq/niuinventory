ba1047375e06ce9d25724f5da5bf31c4b547feb3
Doug Quesnel <dougquesnel@gmail.com>
2013-05-02 10:38:00 -0500

    done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Niuinventory::Application.config.secret_token = '6fd073c003b22b906364a6aa0513093d890aa8e846e299ea14e8265e58a56c7a71f944fd8226ed3fae51d5828907cbd90365ded5b91c73fbfbeefdcd010890a0'
